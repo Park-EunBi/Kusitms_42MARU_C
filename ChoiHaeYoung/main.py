4ef0da586163f2c0b615ac751dc66fc5130b465e
hyoung <ycat01@naver.com>
2022-09-15 13:47:56 +0900

    [FEAT] : startsign ~licenseplate_number
    세부사향 수정 필요

--- ChoiHaeYoung/main.py
+++ ChoiHaeYoung/main.py
@@ -8,11 +8,29 @@ date_lunar = re.compile(r'음력\s(\d+년\s?)?(\d+월\s?)?(\d+일\s?)?') #올해
 date_period_lunar = re.compile(r'음력\s\d+월')
 
 
+fortune_starsign = re.compile('[ㄱ-ㅣ가-힣]+자리')
+fortune_zodiac = re.compile('[ㄱ-ㅣ가-힣]+띠') #뱀ㅁ띠 -> 이런것도 인식되는데 바꿔야하나?
+currencyname = re.compile('([ㄱ-ㅣ가-힣]+달러)|(엔|유로|위안)') #명칭 엄청 다양하게 많음. 사전사용?
+currency_code = re.compile('[A-Z]{3}')
+url = re.compile('(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?')
+bussiness_number = re.compile('[0-9]{3}-[0-9]{2}-[0-9]{5}]')
+phone_number = re.compile('01[016789]-[0-9]{4}-[0-9]{4}]')
+licenseplate_number = re.compile('[0-9]{2,3}\s[ㄱ-ㅣ가-힣][0-9]{4}')
+
 regexes = {
     '@sys.date' : date,
     '@sys.date.period' : date_period,
     '@sys.date.lunar' : date_lunar,
     '@sys.date.period.lunars' : date_period_lunar,
+    '@sys.fortune.starsign' : fortune_starsign,
+    '@sys.fortune.zodiac' : fortune_zodiac,
+    '@sys.currencyname' : currencyname,
+    '@sys.currency.code' : currency_code,
+    '@sys.url' : url,
+    '@sys.bussiness.number' : bussiness_number,
+    '@sys.phone.number' : phone_number,
+    '@sys.licenseplate.number' : licenseplate_number,
+
 }
 
 def Regex(text):
